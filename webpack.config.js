e422441c7a1b87bd187ab5e0a547c3f138a75f6d
fernandaatim <143547756+fernandaatim@users.noreply.github.com>
2025-03-07 14:24:43 -0300

    :lipstick:Refactor SCSS files

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,36 @@ const path = require('path'); //CommonJS
 
 module.exports = {
     mode: 'production',
-    entry: './src/main.js',
+    entry: './src/main.js', // Ponto de entrada
     output: {
-      path: path.resolve(__dirname, 'public', 'assets', 'js'),
-      filename: 'bundle.js'
+      path: path.resolve(__dirname, 'public', 'assets', 'js'), // Diretório de saída
+      filename: 'bundle.js', // Arquivo de saída
     },
     module: {
-      rules: [{
-        exclude: /node_modules/,
-        test: /\.js$/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/env']
-          }
-        }
-      }, {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }]
+      rules: [
+        {
+          exclude: /node_modules/,
+          test: /\.js$/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/env'],
+            },
+          },
+        },
+        {
+          test: /\.scss$/, // Adicionando a regra para arquivos .scss
+          use: [
+            'style-loader',
+            'css-loader', 
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
     },
-    devtool: 'source-map'
-  };
-  
+    devtool: 'source-map',
+};
